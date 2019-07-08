fb694eb6ffa324c72c7620a9158834e6ef1cb729
Wesley Scholl <clients@konjoweb.com>
2019-07-08 17:50:31 -0400

    still configuring state to conditional render parts of the layout

--- mybuzzreact/src/User.js
+++ mybuzzreact/src/User.js
@@ -86,6 +86,7 @@ class User extends Component {
             }
             )
             )
+        console.log(this.state)
         return (
             <div>
                 <div className="container-card">
