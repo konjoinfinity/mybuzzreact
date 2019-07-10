db3e478c3200df4dbbbefab4e454dd9f48973e85
Wesley Scholl <clients@konjoweb.com>
2019-07-10 15:56:00 -0400

    updating react version to utilize jwt auth login

--- mybuzzreact/src/App.js
+++ mybuzzreact/src/App.js
@@ -9,55 +9,150 @@ import Signup from './Signup';
 import Login from './Login';
 import About from './About';
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <main className="container">
-        <Switch>
-          <Route
-            path="/"
-            exact
-            render={props => (
-              <Home {...props} />
-            )}
-          />
-          <Route
-            path="/login"
-            exact
-            render={props => (
-              <Login
-                {...props}
-              />
-            )} />
-          <Route
-            path="/signup"
-            exact
-            render={props => (
-              <Signup
-                {...props}
-              />
-            )} />
-          <Route
-            path="/about"
-            exact
-            render={props => (
-              <About
-                {...props}
-              />
-            )} />
-          <Route
-            path="/user/:id"
-            exact
-            render={props => (
-              <User
-                {...props}
-              />
-            )} />
-        </Switch>
-      </main>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      communities: "",
+      email: "",
+      password: "",
+      confirmpass: "",
+      isLoggedIn: false,
+      error: ""
+    };
+    this.handleLogOut = this.handleLogOut.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.handleLogIn = this.handleLogIn.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+  }
+
+  componentDidMount() {
+    if (localStorage.token) {
+      this.setState({
+        isLoggedIn: true
+      });
+    } else {
+      this.setState({
+        isLoggedIn: false
+      });
+    }
+  }
+
+  handleLogOut(e) {
+    e.preventDefault();
+    this.setState({
+      email: "",
+      password: "",
+      isLoggedIn: false
+    });
+    localStorage.clear();
+    console.log("User has been logged out");
+    var username = localStorage.getItem("username");
+    console.log(username);
+    this.props.history.push("/login");
+  }
+
+  handleInput(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSignUp(e) {
+    e.preventDefault();
+    axios
+      .post(backendUrl + "users/signup", {
+        email: this.state.email,
+        password: this.state.password,
+        confirmpass: this.state.confirmpass
+      })
+      .then(response => {
+        if (response.data.error) {
+          console.log(response.data.error);
+          this.setState({ error: response.data.error });
+        } else {
+          localStorage.token = response.data.token;
+          this.setState({ isLoggedIn: true });
+          console.log("User has signed up");
+          localStorage.setItem("username", this.state.email);
+          this.props.history.push("/");
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
+  handleLogIn(e) {
+    e.preventDefault();
+    axios
+      .post(backendUrl + "users/login", {
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(response => {
+        if (response.data.error) {
+          console.log(response.data.error);
+          this.setState({ error: response.data.error });
+        } else {
+          localStorage.token = response.data.token;
+          this.setState({ isLoggedIn: true });
+          console.log("User is logged in");
+          localStorage.setItem("username", this.state.email);
+          this.props.history.push("/");
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <main className="container">
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={props => (
+                <Home {...props} />
+              )}
+            />
+            <Route
+              path="/login"
+              exact
+              render={props => (
+                <Login
+                  {...props}
+                />
+              )} />
+            <Route
+              path="/signup"
+              exact
+              render={props => (
+                <Signup
+                  {...props}
+                />
+              )} />
+            <Route
+              path="/about"
+              exact
+              render={props => (
+                <About
+                  {...props}
+                />
+              )} />
+            <Route
+              path="/user/:id"
+              exact
+              render={props => (
+                <User
+                  {...props}
+                />
+              )} />
+          </Switch>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default withRouter(App);
--- mybuzzreact/src/Login.js
+++ mybuzzreact/src/Login.js
@@ -3,8 +3,41 @@ import React, { Component } from 'react';
 class Login extends Component {
     render() {
         return (
-            <div>
-                <h1>This is the Login Page</h1>
+            <div className="card m-5">
+                <div className="card-body">
+                    <div className="card-content">
+                        <h2>Log In</h2>
+                        <form>
+                            <div>
+                                <p>Email</p>
+                                <p>
+                                    <input
+                                        type="email"
+                                        name="username"
+                                        onChange={this.props.handleInput}
+                                    />
+                                </p>
+                            </div>
+                            <div>
+                                <p>Password</p>
+                                <p>
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        onChange={this.props.handleInput}
+                                    />
+                                </p>
+                            </div>
+                            <button
+                                className="btn orange lighten-1"
+                                type="submit"
+                                onClick={this.props.handleLogIn}
+                            >
+                                Log In
+                </button>
+                        </form>
+                    </div>
+                </div>
             </div>
         );
     }
