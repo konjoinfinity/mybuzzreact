c70d9af45d48084d516d87737613800680d98906
Wesley Scholl <clients@konjoweb.com>
2019-07-11 15:55:36 -0400

    updated production url to correct deployed heroku url

--- mybuzzreact/src/Urls.js
+++ mybuzzreact/src/Urls.js
@@ -1,5 +1,5 @@
 const devProdUrl = window.location.host.includes("localhost")
     ? "http://localhost:5000/"
-    : "https://mybuzz.herokuapp.com/";
+    : "https://mybuzzin.herokuapp.com/";
 
 export default devProdUrl;
